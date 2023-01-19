152e644e31613fbda3346b4f9ed3a3fcdb8d906c
ashok kumar <spider.akm@gmail.com>
2023-01-19 13:56:22 +0530

    queryselector

--- main.js
+++ main.js
@@ -33,3 +33,32 @@
 
 // var tagitem = document.getElementsByTagName("li")
 // tagitem[0].innerText="hello"
+// var header = document.querySelector('#main-header')
+// header.style.borderBottom = 'solod 4px #ccc'
+
+// var input = document.querySelector('input')
+// input.value="ashok"
+
+// var submit = document.querySelector('input[type="submit"]')
+// submit.value = "send"
+
+// var item = document.querySelector('.list-group-item');
+
+// item.style.color = "red";
+
+// var lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color = "blue";
+
+// Query selector all
+
+var titles = document.querySelectorAll('.title')
+
+titles[0].style.color = "red";
+titles[1].style.color = "blue";
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
+for(var i=0; i<odd.length; i++) {
+    odd[i].style.backgroundColor="red"
+    even[i].style.backgroundColor="blue"
+}
